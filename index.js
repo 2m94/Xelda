6c55a26d447071594203d3c217b0731013edaa8c
2m94 <duzak2016@gmail.com>
2024-10-30 19:38:19 +0000

    All assets imported

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const canvas = document.getElementsById("gameScreen");  //Global Variable outside of the Game Class
+console.log("Canvas: ", canvas);
+class Game{
+    constructor(){
+
+    }
+
+    //The 3 main methods of this game
+    initialize = () =>{
+
+    }
+    update = () =>{
+        //Handles the changes in values such as position
+    }
+    render = () =>{
+        //Displays the changes in values onto the screen
+    }
+    //initialize, update, and render will setup the game and keep it running
+
+}
